91ad3311f617bf34615d9209d2bbf584eb3c7678
unknown <athaarkap@gmail.com>
2020-02-18 22:57:11 +0700

    first project

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <link rel="stylesheet" type="text/css" href="media.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catat Namamu</title>
+    
+</head>
+<body>
+<center><div class="box_login">
+        <h1 style="text-align:center;text-size:70px">LOGIN</h1>
+            <p style="text-align:center;font-size: 20px;">Silahkan Masukan Data Anda</p>
+        <form action="Menuutama.php" method="GET" style="text-align:center">
+        <!-- memasukan username -->
+		    <label>Nama Pelanggan</label>
+		    <input type="text" name="username" class="form_login" placeholder="Nama Pelanggan">
+        <!-- memasukan password -->
+		    <label>Alamat</label>
+		    <input type="text" name="password" class="form_login" placeholder="Alamat Pelanggan">
+        <!-- tombol login -->
+		    <input type="submit" class="tombol_login" value="SUBMIT">
+            </form>
+        </div></center>
+</body>
+</html>
---
+++ menuutama.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="utama.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indie Eatry</title>
+    <link rel = "Shortcut Icon" href ="icon.png"
+</head>
+<body>
+    <<header>
+        <div class="title">
+        <h1>Our Menu's</h1>
+        </div>
+    </header>
+
+    <div class="content"> 
+        <div class="box">
+        <img src="makanan/KentangGoreng.jpg" alt="Kentang Goreng">
+        <div class="deskripsi">Kentang Goreng</div>
+        </div>
+
+        <div class="box">
+        <img src="makanan/taichan2.jpg" alt="Sate Taichan">
+        <div class="deskripsi">Sate Taichan</div>
+        </div>
+
+        <div class="box">
+        <img src="makanan/katsu.jpg" alt="Chicken Katsu">
+        <div class="deskripsi">Chicken Katsu</div>
+        </div>
+
+        <div class="box">
+        <img src="makanan/pancong.jpeg" alt="Kue Pancong">
+        <div class="deskripsi">Kue Pancong</div>
+        </div>
+
+        <div class="box">
+        <img src="makanan/hotang.jpg" alt="Hotang">
+        <div class="deskripsi">Hotang</div>
+        </div>
+
+        <div class="box">
+        <img src="makanan/anekajus.jpg" alt="Jus">
+        <div class="deskripsi">Aneka Jus</div>
+        </div>
+
+        <div class="box">
+        <img src="makanan/sodagembira.jpg" alt="Sogem">
+        <div class="deskripsi">Soda Gembira</div>
+        </div>
+    </div>
+</body>
+</html>
